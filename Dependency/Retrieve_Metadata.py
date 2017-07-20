a7075f57e9a69c1c21ca77a801068a8e2c575942
dobedobedo <yuhsuan.tu@uqconnect.edu.au>
2017-07-20 16:58:20 +1000

    Update Retrieve_Metadata.py

--- Dependency/Retrieve_Metadata.py
+++ Dependency/Retrieve_Metadata.py
@@ -121,14 +121,6 @@ def Ext_SelectBox():
             if messagebox.askyesno("Exit", "Do you want to quit the application?"):
                 self.destroy()
                 sys.exit(0)
-    def center(toplevel):
-        toplevel.update_idletasks()
-        w = toplevel.winfo_screenwidth()
-        h = toplevel.winfo_screenheight()
-        size = tuple(int(_) for _ in toplevel.geometry().split('+')[0].split('x'))
-        x = w/2 - size[0]/2
-        y = h/2 - size[1]/2
-        toplevel.geometry("%dx%d+%d+%d" % (size + (x, y)))
         
     m=popupWindow()
     m.mainloop()
